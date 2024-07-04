3aa4dc09f921c9cbd166ab68a820759707af42e2
jenish <jenish.s@technorthstar.com>
2024-07-04 18:37:56 +0530

    dkdkf

--- lib/screen/RegisterPage.dart
+++ lib/screen/RegisterPage.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:country_code_picker/country_code_picker.dart';
+//import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
@@ -278,35 +278,35 @@ class _RegisterPage extends State<RegisterPage> {
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 7, bottom: 7, right: 7),
-                        padding: EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(8)),
-                            border: Border.all(color: textColor, width: 0.3),
-                            color: ConstantColors.bgColor),
-                        child: CountryCodePicker(
-                          onChanged: (value) {
-                            countrycode = value.dialCode!;
-                            print("changeval===$countryCode");
-                          },
-                          // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
-                          initialSelection: 'IN',
-                          textStyle: TextStyle(
-                              color: textColor,
-                              fontFamily: Constants.fontsFamily),
-                          // favorite: ['+39', 'FR'],
-                          // countryFilter: ['IT', 'FR'],
-                          showFlagDialog: true,
-                          comparator: (a, b) => b.name!.compareTo(a.name!),
-                          //Get the country information relevant to the initial selection
-                          onInit: (code) {
-                            countrycode = code!.dialCode!;
-                            print(
-                                "on init ${code.name} ${code.dialCode} ${code.name}");
-                          },
-                        ),
-                      ),
+                      // Container(
+                      //   margin: EdgeInsets.only(top: 7, bottom: 7, right: 7),
+                      //   padding: EdgeInsets.symmetric(horizontal: 5),
+                      //   decoration: BoxDecoration(
+                      //       borderRadius: BorderRadius.all(Radius.circular(8)),
+                      //       border: Border.all(color: textColor, width: 0.3),
+                      //       color: ConstantColors.bgColor),
+                      //   child: CountryCodePicker(
+                      //     onChanged: (value) {
+                      //       countrycode = value.dialCode!;
+                      //       print("changeval===$countryCode");
+                      //     },
+                      //     // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                      //     initialSelection: 'IN',
+                      //     textStyle: TextStyle(
+                      //         color: textColor,
+                      //         fontFamily: Constants.fontsFamily),
+                      //     // favorite: ['+39', 'FR'],
+                      //     // countryFilter: ['IT', 'FR'],
+                      //     showFlagDialog: true,
+                      //     comparator: (a, b) => b.name!.compareTo(a.name!),
+                      //     //Get the country information relevant to the initial selection
+                      //     onInit: (code) {
+                      //       countrycode = code!.dialCode!;
+                      //       print(
+                      //           "on init ${code.name} ${code.dialCode} ${code.name}");
+                      //     },
+                      //   ),
+                      // ),
                       Expanded(
                         child: Container(
                           height: MediaQuery.of(context).size.height * 0.07,
